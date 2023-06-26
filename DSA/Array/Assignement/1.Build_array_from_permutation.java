d19345512c925a9d286e99dd4966de3e1809ada3
Bhawna Bharat Mehbubani <bhawnamehbubani@gmail.com>
2023-06-26 21:21:04 +0530

    Add files via upload

---
+++ DSA/Array/Assignement/1.Build_array_from_permutation.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int l=nums.length;
+        int[] n=new int[l];
+        for (int i=0;i<l;i++)
+        {
+            n[i]=(nums[nums[i]]);
+        }
+        return n;
+    }
+}
