12448fbc555509076b1b4c0e9b3e6ce6c4a60f5f
Bhawna Bharat Mehbubani <bhawnamehbubani@gmail.com>
2023-06-30 19:55:49 +0530

    Update 27.Remove Duplicates from Sorted Array.java

--- DSA/Array/Assignement/27.Remove Duplicates from Sorted Array.java
+++ DSA/Array/Assignement/27.Remove Duplicates from Sorted Array.java
@@ -1,13 +1,13 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int i,rd=0;
-        for(i=0;i<nums.length;i++){
-            if(nums[rd]!=nums[i]){
-                rd++;
-                nums[rd]=nums[i];
+        int j,i=0;
+        for(j=0;j<nums.length;j++){
+            if(nums[j]!=nums[i]){
+                nums[i+1]=nums[j];
+                i++;
             }
         }
-        return rd+1;
+        return i+1;
         
     }
 }
